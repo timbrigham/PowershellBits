741a2b995b8a93ec1a497d6e5317c4025c11815a
timbrigham <timbrigham@gmail.com>
2020-08-10 14:31:15 -0500

    Download all files in blob
    Sample script to download all logs in a storage account.

---
+++ Azure/downloadAzureBlobs.ps1
@@ -0,0 +1,29 @@
+# Assumes you have already run Connect-AzureRmAccount
+
+$StorageContext = New-AzureStorageContext -StorageAccountName 'XXXX' -StorageAccountKey 'YYYYYYYY'
+$destination_path = 'C:\Temp'  
+
+
+$containers  = Get-AzureStorageContainer -Context $StorageContext
+New-Item -ItemType Directory -Force -Path $destination_path  
+
+ForEach ( $container in $containers ) {
+ $blobs = Get-AzureStorageBlob -Container $container.Name -Context $StorageContext 
+ 
+ ForEach ( $blob in $blobs ){
+	$blobName = [string] $blob.Name
+	$containerName = $container.Name 
+
+
+	Get-AzureStorageBlobContent -Force -Container $containerName `
+		-Blob $blobName `
+		-Destination $destination_path `
+		-Context $StorageContext  
+
+ }
+ 
+}
+
+
+
+
